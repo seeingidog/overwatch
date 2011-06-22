012c6779c06a453fad6887aa94b9295d92b430cf
Dan Ryan <hi@iamdanryan.com>
2011-06-21 20:28:37 -0400

    better organize resque jobs

---
+++ lib/overwatch/jobs/check_run.rb
@@ -0,0 +1,12 @@
+module Overwatch
+  class CheckRun
+    @queue = :checks
+    
+    def self.perform(resource_id)
+      resource = Resource.get(resource_id)
+      resource.checks.each do |check|
+        check.run(resource.last_update)
+      end
+    end
+  end
+end
---
+++ lib/overwatch/jobs/snapshot_reaper.rb
@@ -0,0 +1,10 @@
+module Overwatch
+  class SnapshotReaper
+    @queue = :snapshot_reapers
+
+    def self.perform(snapshot)
+      snapshot = Overwatch::Snapshot.get(snapshot['id'])
+      snapshot.destroy
+    end
+  end
+end
